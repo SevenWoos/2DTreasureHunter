111221d02cf3f450b3a85e99ef03bb02a7255228
7Woos <swenwu96@gmail.com>
2024-12-14 22:49:13 -0800

    Mark 2 - Game Loop & Key Input (Delta Method)

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -74,44 +74,44 @@ public class GamePanel extends JPanel implements Runnable {
 
 	@Override
 	public void run() {
-
-		double drawInterval = 1000000000/ FPS;
-		// When we hit "nextDrawTime", draw the screen again.
-		double nextDrawTime = System.nanoTime() + drawInterval;
+		
+		double drawInterval = 1000000000 / FPS;
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+		long timer = 0;
+		int drawCount = 0;
+		
 		
 		while(gameThread != null) {
 			
-//			System.out.println("The game loop is running.");
+			// Check the current time
+			currentTime = System.nanoTime();
 			
+			// Subtract "lastTime" from "currentTime" to get the time elapsed.
+			// We then divide the time elapsed by drawInterval
+			// Increase delta by this amount
+			delta += (currentTime - lastTime) / drawInterval;
+			timer += (currentTime - lastTime);
 			
-			 long currentTime = System.nanoTime();
-			 System.out.println("Current time: " + currentTime);
-			 
+			lastTime = currentTime;
 			
-			update();
-			
-			repaint();
-			
-			try {
-				double remainingTime = nextDrawTime - System.nanoTime();
-
-				remainingTime = remainingTime / 1000000;
-				
-				if(remainingTime < 0) {
-					remainingTime = 0;
-				}
-				
-				Thread.sleep((long) remainingTime);
-				
-				// Set NEW "nextDrawTime"
-				nextDrawTime += drawInterval;
-				
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			if(delta >= 1) {
+				update();
+				repaint();
+				delta--;
+				drawCount++;
 			}
+			
+			// When timer reaches 1 second(1 billion nanoseconds)
+			if(timer >= 1000000000) {
+				System.out.println("FPS: " + drawCount);
+				drawCount = 0;
+				timer = 0;
+			}
+					
+			
 		}
-		
 	}
 	
 	// Update player position
